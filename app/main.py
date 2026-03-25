425b443aecb3dc659ce8c65629e1b1dc24f1314a
Tran Khanh An <khanhan2007.tran@gmail.com>
2026-03-25 15:07:40 +0200

    ip change

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
+from fastapi import Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello an!", "v": "0.2" }
+    return { "msg": "Hello an!", "v": "0.3" }
 
 
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
+app = FastAPI()
+
+
+my_name= "an"
+@app.get("/")
+def read_root():
+    local_var = "Hello"
+    return {"msg": "Hello " +my_name}
+
+@app.get("/api/ip")
+def get_ip_json(request: Request):
+    ip=request.client.host
+    return {"ip" : ip}
